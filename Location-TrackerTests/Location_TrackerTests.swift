aa6a59a801aa44691f35bcc761cd2bff7562699d
Benjamin Lavin <bennybenzapps@gmail.com>
2014-06-17 15:10:57 -0600

    Initial Commit

---
+++ Location-TrackerTests/Location_TrackerTests.swift
@@ -0,0 +1,35 @@
+//
+//  Location_TrackerTests.swift
+//  Location-TrackerTests
+//
+//  Created by Benjamin Lavin on 2014-06-17.
+//  Copyright (c) 2014 Benjamin Lavin. All rights reserved.
+//
+
+import XCTest
+
+class Location_TrackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
